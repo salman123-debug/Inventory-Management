b59378678d43649fad271b3c489b9415f2eb94ff
Sachinky53 <120941174+Sachinky53@users.noreply.github.com>
2024-12-18 18:52:03 +0530

    working on navbar in Header component
    few things is pending like dark light theme and all are done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
-
+import Home from './Components/Home'
+import { useState } from 'react'
 function App() {
-
-
+const  [theme,setTheme]=useState("light")
   return (
-    <>
-      
-    </>
+    <div className="container">
+      <Home theme={theme} setTheme={setTheme} />
+      </div>
   )
 }
 
