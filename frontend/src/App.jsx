c00a98f7e46f9995d7052439e339458ab949d53f
Sachinky53 <120941174+Sachinky53@users.noreply.github.com>
2024-12-19 23:27:38 +0530

    main component is added
    nav bar and main components is added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
-
-import './App.css'
+import React from 'react'
 import Home from './Components/Home'
-import { useState } from 'react'
 function App() {
-const  [theme,setTheme]=useState("light")
   return (
-    <div className="container">
-      <Home theme={theme} setTheme={setTheme} />
-      </div>
+    <>
+<Home />    
+    </>
   )
 }
 
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Header from './Header'
-
+import Main from './Main'
 function Home() {
   return (
-    <>
-    
-    <Header />
-    
-    </>
+    <div>
+      <Header />
+      <Main />
+    </div>
   )
 }
 
