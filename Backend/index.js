932d04c511b0ba590fac8c2cbe3c217a5c740f54
salman123-debug <muhdsalman2510@gmail.com>
2025-02-07 14:23:48 +0530

    small changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const dotenv = require('dotenv')
